678d044c254054ccc8070488ef6d0d1b396d6927
nwrm <woramet.son@gmail.com>
2024-06-19 20:41:45 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
         title: 'XO',
         home: GameScreen(),
-        debugShowCheckedModeBanner: true,
+        debugShowCheckedModeBanner: false,
       );
   }
 }
